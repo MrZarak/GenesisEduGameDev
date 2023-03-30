579c5a00f094ddad83d38cea45bf0f889eec4f61
Dmytro Kolodyazhniy <d.kolodyazhniy@geniusee.com>
2023-03-30 16:43:26 +0300

    Feat

---
+++ Assets/Scripts/UIMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using moving;
+using moving.impl;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIMenu : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private UIInputSource inputSource;
+    [SerializeField] private InputCode inputCode;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(1);
+        inputSource.UpdateState(inputCode, false);
+    }
+
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        inputSource.UpdateState(inputCode, true);
+    }
+}
